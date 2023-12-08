607f8ae998b7f6d3b1faaf56da5838bd6d1b089f
Aurits <alandaambrose@gmail.com>
2023-12-08 06:21:29 +0000

    Dec 8, 2023, 9:21 AM

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 #producs model with name,price,stock and image fields
@@ -7,6 +8,7 @@ class Product(models.Model):
     price=models.DecimalField(max_digits=7,decimal_places=2)
     stock=models.IntegerField()
     image=models.ImageField(upload_to='products',blank=True)
+    owner=models.ForeignKey(User,on_delete=models.CASCADE, related_name='products', null=True)
     #string representation of the model
     def __str__(self):
         return self.name
