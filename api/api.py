7c4db11b199f142b4ac5499a654d7034e2840881
Aurits <alandaambrose@gmail.com>
2023-12-08 06:28:14 +0000

    Dec 8, 2023, 9:28 AM

--- api/api.py
+++ api/api.py
@@ -3,8 +3,16 @@ from rest_framework import viewsets,permissions
 from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset=Product.objects.all()
+    
     permission_classes=[
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
     serializer_class=ProductSerializer
+
+    def get_queryset(self):
+        return self.request.user.products.all()
+    
+    def perform_create(self,serializer):
+        serializer.save(owner=self.request.user)
+
+    
